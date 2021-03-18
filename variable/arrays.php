5440dc133e566e71c791aeb2f0bcca6d16e9566f
Michael <michael.zerghe@gmail.com>
2021-03-18 16:40:32 +0100

    up

---
+++ variable/arrays.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$family = array('father','step-mother','sister','mother','step-father');
+print_r($family);
+$recipes = array('tiramisu','pizza','frites','poulet');
+print_r($recipes);
+$movies = array('back to the future','space jam','fight club','avengers','iron man');
+
+print_r($movies[0]);
+
+?>
+
+</body>
+</html>
